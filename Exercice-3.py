0f5b4cd42e78a2e818fe40481e76641f75f61102
Fidel Korti-Frost <148016801+korti-frost@users.noreply.github.com>
2023-10-16 01:24:30 +0200

    Create Exercice-3.py
    Code de d'une solutions possible de l'exercice 3, partie 3.

---
+++ Exercice-3.py
@@ -0,0 +1,15 @@
+import boto3
+
+def hello_s3():
+    s3_resource = boto3.resource('s3')
+    print("Buckets:")
+    for bucket in s3_resource.buckets.all():
+   	 print(f"\t{bucket.name}")
+    
+    name_bucket = s3_resource.Bucket(bucket.name)
+    for name_bucket in name_bucket.objects.all():
+   	 print(name_bucket)
+    name_bucket.delete()
+
+if __name__ == '__main__':
+    hello_s3()
